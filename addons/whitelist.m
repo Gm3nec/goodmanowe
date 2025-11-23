bf955064b9c1f98d2dffa37e77449a37156d741e
Gm3nec <josef.heczko@email.cz>
2025-11-23 07:34:14 +0100

    Add files via upload

---
+++ addons/whitelist.m
@@ -0,0 +1,8 @@
+; // vim: set ft=tf:
+
+;;;;WHITELIST - just add names, if u change line do a \;;;;
+/set whitelist %whitelist Greenknight
+
+
+;;;;BLACKLIST - same same
+/set blacklist %blacklist Flash
